047a4b4eb9456db15410cf62ae9f3a7a4b5679af
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-10-14 18:37:50 -0300

    feat: finish EditPost layout

--- client/src/components/EditPost.js
+++ client/src/components/EditPost.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { editPost, fetchPost } from '../actions/PostActions'
 import { fetchCategories } from '../actions/CategoryActions'
+import { isEmpty } from '../utils/helpers'
+import { Row, Col, Button, FormGroup, ButtonToolbar, FormControl, ControlLabel } from 'react-bootstrap'
+import { FieldGroup } from './widget/FieldGroup'
 
 class EditPost extends Component {
   componentWillMount() {
@@ -17,6 +20,10 @@ class EditPost extends Component {
   }
 
   state = {
+    error: {
+      title: false,
+      body: false,
+    },
     post: {
       title: "",
       body: "",
@@ -27,47 +34,100 @@ class EditPost extends Component {
     category: this.props.match.params.category,
   }
 
-  render() {
-    const { post, postId } = this.state
-    const { categories, editPost } = this.props
+  validateAndEditPost(id, post) {
+    const error = {
+      title: isEmpty(post.title),
+      body: isEmpty(post.body),
+    }
 
-    if (!post) {
+    this.setState({
+      ...this.state,
+      error
+    })
+
+    if (!(error.title || error.body)) {
+      this.props.editPost(id, post).then((post) => {
+        if (post) {
+          this.props.history.push(`/${post.category}/${post.id}`)
+        } else {
+          this.props.history.push("/")
+        }
+      })
+    }
+  }
+
+  render() {
+    const { post, postId, error } = this.state
+    const { categories } = this.props
+
+    if (isEmpty(post)) {
       return (
-        <div>
-          <h2>Post not Found</h2>
-        </div>
+        <Col xs={12}>
+          <Row>
+            <Col xs={12}>
+              <h2>Post not Found</h2>
+            </Col>
+          </Row>
+        </Col>
       )
     } else {
       return (
-        <div>
-          <h2>Edit Post</h2>
+        <Col xs={12}>
+          <Row>
+            <Col xs={12}>
+              <h2>Edit Post</h2>
+            </Col>
+          </Row>
           <form>
-            <div>
-              <label htmlFor="title">Title:</label>
-              <input type="text" id="title" placeholder="Title" value={post.title} onChange={(event) => this.setState({
+            <FieldGroup
+              id="title"
+              type="text"
+              label="Title"
+              placeholder="Title"
+              validationState={error.title ? "error" : null}
+              value={post.title}
+              onChange={(event) => this.setState({
+                error: {
+                  ...error,
+                  title: false
+                },
                 post: {
                   ...post,
                   title: event.target.value
                 }
-              })} />
-            </div>
-            <div>
-              <label htmlFor="body">Content:</label>
-              <textarea id="body" placeholder="Content" value={post.body} onChange={(event) => this.setState({
+              })}
+            />
+            <FieldGroup
+              id="body"
+              type="text"
+              label="Content"
+              placeholder="Content"
+              validationState={error.body ? "error" : null}
+              componentClass="textarea"
+              value={post.body}
+              onChange={(event) => this.setState({
+                error: {
+                  ...error,
+                  body: false
+                },
                 post: {
                   ...post,
                   body: event.target.value
                 }
-              })} />
-            </div>
-            <div>
-              <label htmlFor="author">Author:</label>
-              <input type="text" id="author" disabled={true} placeholder="Author" value={post.author} />
-            </div>
-            <div>
-              <label htmlFor="sortSelector">Category:</label>
-              <select id="sortSelector" disabled={true} value={post.category}>
-                <option value="">Select a category</option>
+              })}
+            />
+            <FieldGroup
+              id="author"
+              type="text"
+              label="Author"
+              placeholder="Author"
+              value={post.author}
+              disabled={true}
+            />
+            <FormGroup>
+              <ControlLabel>Category</ControlLabel>
+              <FormControl id="sortSelector" disabled={true} componentClass="select" value={post.category} >
+                <option value="" defaultValue>Select a category</option>
                 {categories && categories.map((category) => (
                   category.name !== "all" &&
                   <option
@@ -76,24 +136,20 @@ class EditPost extends Component {
                     {category.name}
                   </option>
                 ))}
-              </select>
-            </div>
-            <div><input type="submit" value="Edit" onClick={(event) => {
-              event.preventDefault()
-              editPost(post.id, { title: post.title, body: post.body }).then((post) => {
-                if (post) {
-                  this.props.history.push(`/${post.category}/${post.id}`)
-                } else {
-                  this.props.history.push("/")
-                }
-              })
-            }} /></div>
-            <div><input type="button" value="Cancel" onClick={(event) => {
-              event.preventDefault()
-              this.props.history.push(`/${post.category}/${postId}`)
-            }} /></div>
+              </FormControl>
+            </FormGroup>
+            <ButtonToolbar>
+              <Button bsStyle="success" type="submit" onClick={(event) => {
+                event.preventDefault()
+                this.validateAndEditPost(post.id, { title: post.title, body: post.body })
+              }}>Edit</Button>
+              <Button bsStyle="danger" type="submit" value="Cancel" onClick={(event) => {
+                event.preventDefault()
+                this.props.history.push(`/${post.category}/${postId}`)
+              }}>Cancel</Button>
+            </ButtonToolbar>
           </form>
-        </div>
+        </Col>
       )
     }
   }
@@ -110,12 +166,17 @@ const mapStateToProps = (state) => {
     newPost = {
       ...state.posts[0]
     }
-  }
 
-  return {
-    categories: state.categories,
-    post: {
-      ...newPost
+    return {
+      categories: state.categories,
+      post: {
+        ...newPost
+      }
+    }
+  } else {
+    return {
+      categories: state.categories,
+      post: null
     }
   }
 }
